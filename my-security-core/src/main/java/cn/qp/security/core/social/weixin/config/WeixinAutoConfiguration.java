165e90a9dabeb1d468fad410af7eb9fb89048283
baozi1110 <743604512@qq.com>
2019-10-15 16:36:19 +0800

    绑定与解绑

--- my-security-core/src/main/java/cn/qp/security/core/social/weixin/config/WeixinAutoConfiguration.java
+++ my-security-core/src/main/java/cn/qp/security/core/social/weixin/config/WeixinAutoConfiguration.java
@@ -4,12 +4,16 @@ package cn.qp.security.core.social.weixin.config;
 
 import cn.qp.security.core.properties.SecurityProperties;
 import cn.qp.security.core.properties.WeixinProperties;
+import cn.qp.security.core.social.ImoocConnectView;
 import cn.qp.security.core.social.weixin.connect.WeixinConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.social.SocialAutoConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.web.servlet.View;
 
 /**
  * 微信登录配置
@@ -29,6 +33,16 @@ public class WeixinAutoConfiguration extends SocialAutoConfigurerAdapter {
 		WeixinProperties weixinConfig = securityProperties.getSocial().getWeixin();
 		return new WeixinConnectionFactory(weixinConfig.getProviderId(), weixinConfig.getAppId(),
 				weixinConfig.getAppSecret());
+	}
+
+	/**
+	 * 绑定成功的返回视图
+	 * @return 返回了通用的信息，可以自定义
+	 */
+	@Bean({"connect/weixinConnect", "connect/weixinConnected"})
+	@ConditionalOnMissingBean(name = "weixinConnectedView")
+	public View weixinConnectedView() {
+		return new ImoocConnectView();
 	}
 
 }
